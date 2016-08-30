6b744b33e85b7a7c6db0234a6c8cfe00d6cc9816
tjschiffer <tjschiffer@gmail.com>
2016-08-29 20:15:30 -0400

    Add basic random logic and size fixes

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -21,7 +21,7 @@ var tic_tac_toe = (function() {
 		$(tttSelector).click(function(e) {
 			var target = $(e.target);
 				loc = target.data('ttt-loc').toString().split('');
-			if (target.attr('class').indexOf('ttt-marked-') > -1) return;
+			if (target.prop('class').indexOf('ttt-marked-') > -1) return;
 			var square = tttMap, lastCoord;
 			$.each(loc, function(i, coord) {
 				if (square[coord]) square = square[coord];
@@ -30,18 +30,23 @@ var tic_tac_toe = (function() {
 			square[lastCoord] = 'X';
 			target.addClass('ttt-marked-X');
 
-			var square = tttMap, lastCoord, j = 9, dataLoc = '';
-			while (j--) {
-				var rand = getRandomInt(0, size);
-				dataLoc += rand.toString();
-				if (square[rand]) {
-					square = square[rand];
-					lastCoord = rand;
-				} else {
-					break;
+			var j = 8;
+			while (square[lastCoord] && j--) {
+				var square = tttMap, lastCoord, dataLoc = '';
+				while (true) {
+					var rand = getRandomInt(0, size),
+						lastCoord = rand;
+					dataLoc += rand.toString();
+					if (square[rand] && ['X', 'O'].indexOf(square[rand]) == -1) {
+						square = square[rand];
+					} else {
+						break;
+					}
 				}
 			}
+
 			if (!(square[lastCoord])) {
+				square[lastCoord] = "O";
 				$('.tic-tac-toe [data-ttt-loc=' + dataLoc + ']').addClass('ttt-marked-O');
 			}
 		});
